53bbb382821eb495b16a6f7a790057094d4ba838
Viktor Linné <viktor.linne@medieinstitutet.se>
2025-01-08 11:41:57 +0100

    fixed missing import for Menu.jsx in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Dishes from './components/Dishes'
 import About from "./components/About.jsx";
+import Menu from './components/Menu'
 
 function App() {
     return (
