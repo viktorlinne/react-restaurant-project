b356309648c9793d395aa6a315774a4ce12f531e
Viktor Linné <viktor.linne@medieinstitutet.se>
2025-01-08 09:30:59 +0100

    added image for About.jsx component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import about from 'src/assets/about.png'
 
 const About = () => {
     return (
-
+        <div>
+            <img src={about} alt=""/>
+        </div>
     )
 }
 
