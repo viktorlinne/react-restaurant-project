8d1180c0383fbc4c780020ddf33c789af2595383
Viktor Linné <viktor.linne@medieinstitutet.se>
2025-01-08 13:49:42 +0100

    added linking to top of page functionality for La Mamas text in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
                         <span>
                             <BiRestaurant size={32}/>
                         </span>
-                        <h1 className="text-xl font-semibold">La Mamas</h1>
+                        <Link to={"home"} spy={true} smooth={true} duration={500}><h1  className="text-xl font-semibold hover:text-brightColor transition-all cursor-pointer">La Mamas</h1></Link>
                     </div>
                     <nav className="hidden md:flex flex-row items-center text-lg font-medium gap-8">
                         <Link to={"home"} spy={true} smooth={true} duration={500}
@@ -36,20 +36,20 @@ const Navbar = () => {
                                 <Link to={"menu"} spy={true} smooth={true} duration={500}
                                       className={"hover:text-brightColor transition-all cursor-pointer"}>Meny</Link>
 
-                                <BiChevronDown className={"cursor-pointer"} size={25} />
+                                <BiChevronDown className={"cursor-pointer"} size={25}/>
 
                             </div>
                             <ul className="absolute hidden space-y-2 group-hover:block bg-white border border-gray-300 rounded-lg p-5">
 
-                                <li><Link to={"pizzor"} spy={true} smooth={true} duration={500}
+                                <li><Link to={"pizzas"} spy={true} smooth={true} duration={500}
                                           className={"hover:text-brightColor transition-all cursor-pointer"}>Pizzor</Link>
                                 </li>
 
-                                <li><Link to={"rullar"} spy={true} smooth={true} duration={500}
+                                <li><Link to={"rolls"} spy={true} smooth={true} duration={500}
                                           className={"hover:text-brightColor transition-all cursor-pointer"}>Rullar</Link>
                                 </li>
 
-                                <li><Link to={"sallader"} spy={true} smooth={true} duration={500}
+                                <li><Link to={"sallads"} spy={true} smooth={true} duration={500}
                                           className={"hover:text-brightColor transition-all cursor-pointer"}>Sallader</Link>
                                 </li>
                             </ul>
@@ -57,7 +57,7 @@ const Navbar = () => {
                         <Link to={"about"} spy={true} smooth={true} duration={500}
                               className={"hover:text-brightColor transition-all cursor-pointer"}>Om oss</Link>
 
-                        <Link to={"hitta-oss"} spy={true} smooth={true} duration={500}
+                        <Link to={"find-us"} spy={true} smooth={true} duration={500}
                               className={"hover:text-brightColor transition-all cursor-pointer"}>Hitta Oss</Link>
 
                         <Link to={"review"} spy={true} smooth={true} duration={500}
@@ -76,8 +76,9 @@ const Navbar = () => {
                         }
                     </div>
                 </div>
-                <div
-                    className={`${menu ? "translate-x-0" : "-translate-x-full"} lg:hidden flex flex-col absolute bg-black text-white left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}>
+                <div className={`${menu ? "translate-x-0" : "-translate-x-full"} lg:hidden flex flex-col absolute bg-black 
+                                           text-white left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full 
+                                           h-fit transition-transform duration-300`}>
 
                     <Link to={"menu"} spy={true} smooth={true} duration={500}
                           className={"hover:text-brightColor transition-all cursor-pointer"}>Meny</Link>
