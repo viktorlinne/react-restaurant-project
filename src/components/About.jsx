f85f8f2022f134ada1517a887125d49548c59ed7
Viktor Linné <viktor.linne@medieinstitutet.se>
2025-01-09 13:51:36 +0100

    -added correct link for google maps iframe in About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
                 <iframe
                     id={"find-us"}
                     className={"h-96 w-full rounded-lg shadow-lg"}
-                    src="https://www.google.com/maps/embed?..."
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2144.442148676628!2d12.073869577175577!3d57.48783625703811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464fe94a55409011%3A0xfdcd869443c8942d!2sLa%20Mama%20Pizzeria!5e0!3m2!1ssv!2sse!4v1736427018358!5m2!1ssv!2sse"
                     referrerPolicy="no-referrer-when-downgrade"
                     allowFullScreen
                     style={{ border: "none" }}
