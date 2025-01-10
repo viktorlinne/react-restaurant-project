1611ceaf9a44500528a6151838f74153bfd0b1a5
Viktor Linné <viktor.linne@medieinstitutet.se>
2025-01-10 12:59:55 +0100

    -moved map to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-scroll";
 const Footer = () => {
     return (
         <div className={"bg-nav text-black mt-8"}>
-            <div className={"flex flex-col md:flex-row justify-between p-8 lg:px-32 px-5"}>
+            <div className={"flex text-center flex-col md:flex-row justify-between p-8 lg:px-32 px-5"}>
                 <div className={"w-full md:w-1/4 mt-4"}>
 
                     <h1 className={"font-semibold text-xl pb-4"}>
@@ -16,6 +16,19 @@ const Footer = () => {
                     </p>
 
                 </div>
+
+                <div className={"w-4/5 max-w-3xl mt-4"}>
+
+                    <iframe
+                        className={"h-96 w-full rounded-lg shadow-lg"}
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2144.442148676628!2d12.073869577175577!3d57.48783625703811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464fe94a55409011%3A0xfdcd869443c8942d!2sLa%20Mama%20Pizzeria!5e0!3m2!1ssv!2sse!4v1736427018358!5m2!1ssv!2sse"
+                        referrerPolicy="no-referrer-when-downgrade"
+                        allowFullScreen
+                    >
+                    </iframe>
+
+                </div>
+
                 <div className={"font-medium text-xl pb-4 mt-4"}>
 
                     <nav className={"flex flex-col gap-2"}>
